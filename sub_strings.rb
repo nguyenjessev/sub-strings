779286f6e2bc8d3fd8bbd907a47369669e1d7eec
Jesse <nguyenjessev@gmail.com>
2020-07-26 18:31:15 -0400

    Add substrings method skeleton

---
+++ sub_strings.rb
@@ -0,0 +1,15 @@
+def substrings(text, dictionary)
+
+  # Create an empty hash
+
+  # Loop through words in the dictionary
+
+  # Scan text for occurrences, add values to hash
+
+  # Return hash
+
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
